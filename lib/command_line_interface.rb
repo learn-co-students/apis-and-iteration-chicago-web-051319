ab7ffe08f64c16b79fe3fc4d4190561ba81bec0a
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-14 15:27:23 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,5 +1,8 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to a Star Wars Dictionary"
+  puts "Contains information regarding the canon Star Wars timeline"
+  puts "Please enter input"
 end
 
 def get_character_from_user
