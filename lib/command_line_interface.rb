dd5a32e0f30191d13b2ea0278a17cfab85b58daf
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-14 15:28:34 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -9,6 +9,6 @@ end
 def get_character_from_user
   puts "please enter a character name"
   inp = gets.chomp
-  inp.downcase
+  inp = inp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
