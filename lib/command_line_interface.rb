9b42182cda64ae132278cfb4a1046c911fc670ae
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-14 15:28:15 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,10 +2,13 @@ def welcome
   # puts out a welcome message here!
   puts "Welcome to a Star Wars Dictionary"
   puts "Contains information regarding the canon Star Wars timeline"
-  puts "Please enter input"
+  get_character_from_user
+  
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  inp = gets.chomp
+  inp.downcased
   # use gets to capture the user's input. This method should return that input, downcased.
 end
